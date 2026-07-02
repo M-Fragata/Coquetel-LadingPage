422a59228e036ce5daebc57aebadea64f3ff0702
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-07-02 18:14:20 -0300

    Hero Section pronta

--- Coquetel/src/App.tsx
+++ Coquetel/src/App.tsx
@@ -3,13 +3,15 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { SplitText } from 'gsap/SplitText'
 
 import { NavBar } from './components/Navbar'
-
+import { Hero } from './components/hero'
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 export const App = () => {
     return (
         <main>
             <NavBar />
+            <Hero />
+            <div className="h-dvh bg-black"></div>
         </main>
     )
 }
